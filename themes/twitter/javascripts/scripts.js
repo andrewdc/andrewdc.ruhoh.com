181e5de6d3b70d9ffaa6a6ebfdb23677edbd3b34
Andrew Colclough <andrew.design@gmail.com>
2012-09-05 12:01:41 -0700

    nixed the tb popover for a custom jobber

--- themes/twitter/javascripts/scripts.js
+++ themes/twitter/javascripts/scripts.js
@@ -4,14 +4,31 @@ jQuery(document).ready(function($) {
     effect: "fadeIn"
   });
   
-  $('.thumbnails li:odd').popover({title: "No Title", content: "no data-content", placement: "left", delay:{show: 150, hide: 0}});
-  $('.thumbnails li:even').popover({title: "No Title", content: "no data-content", placement: "right", delay:{show: 150, hide: 0}});
+  //====Thumbnail JS =================================
+  // $('.thumbnails li:odd').popover({title: "No Title", content: "no data-content", placement: "left", delay:{show: 150, hide: 0}});
+  // $('.thumbnails li:even').popover({title: "No Title", content: "no data-content", placement: "right", delay:{show: 150, hide: 0}});
+  
+  // This creates the hover info on gallery items, and is about a bagillion times better than the TB popover I was trying to shoehorn.
+  $('.thumbnails li').each(function(index) {
+    var $info = $(this).find('span.info');
+    var tipheight = $(this).height();
+    $info.css('top', tipheight);
+
+    $(this).hover(function() {
+      $info.stop(1);
+      $info.animate({top: tipheight - $info.height()});
+    }, function() {
+      $info.stop(1);
+      $info.animate({top: tipheight });
+    });
+  });
+
+
 
   $('.modaltrigger').click(function(event) {
     $(this).parents('li').popover('hide');
     var imgurl = $(this).attr('href');
     var thetitle = $(this).attr('name');
-    console.log(imgurl);
     $("#myModal .modal-body").html('<img src='+ imgurl +' />');
     $("#myModalLabel").text(thetitle);
   });
@@ -19,7 +36,6 @@ jQuery(document).ready(function($) {
   $('.oldsite').click(function(event) {
     var imgurl = $(this).attr('href');
     var thetitle = $(this).attr('name');
-    console.log(imgurl);
     $("#myModal .modal-body").html('<img src='+ imgurl +' />');
     $("#myModalLabel").text(thetitle);
   });
@@ -38,7 +54,7 @@ $('#jobs li').click(function(event) {
 
 //===Drawers======================
 $('#skill').toggle(function() {
-  $('.rightwing').show("slide", {direction:"right"}, 100);
+  $('.rightwing').show("slide", {direction:"right"}, 300);
     $('.skills').mCustomScrollbar("update");
   
 }, function() {
@@ -47,7 +63,7 @@ $('#skill').toggle(function() {
 });
 
 $('.res').toggle(function() {
-  $('.leftwing').show("slide", {direction:"left"}, 100);
+  $('.leftwing').show("slide", {direction:"left"}, 300);
   $('.work').mCustomScrollbar("update");
 
 }, function() {
